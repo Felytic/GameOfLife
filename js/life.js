acf140ff304c874577022277aac62b2298bf1581
Felytic <felytic@gmail.com>
2016-01-10 14:50:34 +0200

    Refactoring

--- js/life.js
+++ js/life.js
@@ -2,63 +2,86 @@
 var lifeCells = [
   [0]
 ];
-var STEP = 20;
+var CELL_SIZE = 20;
 var timer;
+
+$(window).load(function() {
+  resizeCanvas($('#lifeCanvas')[0], CELL_SIZE);
+  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+});
+
+$(window).resize(function() {
+  resizeCanvas($('#lifeCanvas')[0], CELL_SIZE);
+  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+});
+
 $('.play-pause').click(function() {
   $(this).find(':first-child').toggleClass('glyphicon-play');
   $(this).find(':first-child').toggleClass('glyphicon-pause');
 });
 
-$(window).load(function() {
-  resizeCanvas($('#lifeCanvas')[0], STEP);
-  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], STEP);
-  drawCells(lifeCells, $('#lifeCanvas')[0], STEP);
+$('#playButton').click(function() {
+  if (!timer) {
+    timer = setTimeout(function tick() {
+      nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+      timer = setTimeout(tick, 500 / $('#speedInput').val());
+      // "500 / $('#speedInput').val()" duplicates because it should be calculated on each step
+      //              ^ this value can be changed by user while animation
+    }, 500 / $('#speedInput').val());
+  } else {
+    clearTimeout(timer);
+    timer = null;
+  }
 });
 
-$(window).resize(function() {
-  resizeCanvas($('#lifeCanvas')[0], STEP);
-  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], STEP);
-  drawCells(lifeCells, $('#lifeCanvas')[0], STEP);
+$('#lifeCanvas').click(function(pos) {
+  var canvas = $(this);
+  var x = pos.clientX - canvas.position().left;
+  var y = pos.clientY - canvas.position().top;
+
+  //Ajust to the center of clicked circle
+  x = x - x % CELL_SIZE + CELL_SIZE / 2;
+  y = y - y % CELL_SIZE + CELL_SIZE / 2;
+
+  //Position in cells matrix
+  var i = Math.floor(x / CELL_SIZE);
+  var j = Math.floor(y / CELL_SIZE);
+
+  lifeCells[i][j] = +!lifeCells[i][j];
+  drawCell(this, i, j, lifeCells[i][j], CELL_SIZE);
 });
 
-function fitCellsToCanvas(cellsArray, canvasElement, step) {
-  var canvas = $(canvasElement);
+$('#stepButton').click(function() {
+  nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+});
+
+$('#resetButton').click(function() {
+  lifeCells = [
+    [0]
+  ];
+  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+});
+
+function fitCellsToCanvas(cellsArray, canvasElement, cellSize) {
   var cells = cellsArray;
-  var cellsX = canvas.width() / step;
-  var cellsY = canvas.height() / step;
-  if (cellsX > cells.length || cellsY > cells[0].length) {
+  var cellsWidth = canvasElement.width / cellSize;
+  var cellsHeight = canvasElement.height / cellSize;
+  if (cellsWidth > cells.length || cellsHeight > cells[0].length) {
     for (var i = 0; i < cells.length; i++) {
-      cells[i].push.apply(cells[i], Array.apply(null, new Array(cellsY - cells[i].length)).map(e => 0));
+      cells[i].push.apply(cells[i], Array.apply(null, new Array(cellsHeight - cells[i].length)).map(e => 0));
     }
-    cells.push.apply(cells, Array.apply(null, new Array(cellsX - cells.length)).map(
-      e => Array.apply(null, new Array(cellsY)).map(e => 0)));
+    cells.push.apply(cells, Array.apply(null, new Array(cellsWidth - cells.length)).map(
+      e => Array.apply(null, new Array(cellsHeight)).map(e => 0)));
   }
 }
 
-function drawCells(cellsArray, canvasElement, step) {
-  var canvas = $(canvasElement);
-  var cells = cellsArray;
-  var ctx = canvas[0].getContext('2d');
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(0, 0, canvas.width(), canvas.height());
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = '#000';
-  for (var i = 0; i < cells.length; i++) {
-    for (var j = 0; j < cells[0].length; j++) {
-      var x = i * step + step / 2;
-      var y = j * step + step / 2;
-      ctx.strokeStyle = 'rgb(0,' +
-        Math.floor(100 * i * step / canvas.width() + 130) + ',' +
-        Math.floor(100 * j * step / canvas.height() + 130) + ')';
-      ctx.beginPath();
-      ctx.arc(x, y, step * 0.42, 0, Math.PI * 2, true);
-      if (cells[i][j] == 1) {
-        ctx.fillStyle = 'rgb(0,' + Math.floor(100 * x / canvas.width() + 130) + ',' + Math.floor(100 * y / canvas.height() + 130) + ')';
-      } else {
-        ctx.fillStyle = '#fff';
-      }
-      ctx.fill();
-      ctx.stroke();
+function drawCells(cellsArray, canvasElement, cellSize) {
+  for (var i = 0; i < cellsArray.length; i++) {
+    for (var j = 0; j < cellsArray[0].length; j++) {
+      drawCell(canvasElement, i, j, cellsArray[i][j], cellSize);
     }
   }
 }
@@ -74,33 +97,7 @@ function resizeCanvas(canvasElement, step) {
   canvas[0].height = canvas.height();
 }
 
-$('#lifeCanvas').click(function(pos) {
-  var canvas = $(this);
-  var x = pos.clientX - canvas.position().left;
-  var y = pos.clientY - canvas.position().top;
-  x = x - x % STEP + STEP / 2;
-  y = y - y % STEP + STEP / 2;
-  var ctx = canvas[0].getContext('2d');
-  var cell = lifeCells[Math.floor(x / STEP)][Math.floor(y / STEP)];
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(x - STEP / 2, y - STEP / 2, STEP, STEP);
-  ctx.lineWidth = 1;
-  ctx.strokeStyle = 'rgb(0,' + Math.floor(100 * x / canvas.width() + 130) + ',' + Math.floor(100 * y / canvas.height() + 130) + ')';
-  if (cell == 0) {
-    ctx.fillStyle = 'rgb(0,' + Math.floor(100 * x / canvas.width() + 130) + ',' + Math.floor(100 * y / canvas.height() + 130) + ')';
-    cell = 1;
-  } else {
-    ctx.fillStyle = '#fff';
-    cell = 0;
-  }
-  lifeCells[Math.floor(x / STEP)][Math.floor(y / STEP)] = cell;
-  ctx.beginPath();
-  ctx.arc(x, y, 8.5, 0, 2 * Math.PI, true);
-  ctx.fill();
-  ctx.stroke();
-});
-
-function nextPopulation(cellsArray) {
+function nextPopulation(cellsArray, canvasElement, cellSize) {
   var cells = cellsArray.map(e => e.slice());
   for (var i = 0; i < cells.length; i++) {
     for (var j = 0; j < cells[0].length; j++) {
@@ -112,30 +109,39 @@ function nextPopulation(cellsArray) {
       }
       sum -= cells[i][j];
       if (cells[i][j]) {
-        if (sum < 2 || sum > 3) cellsArray[i][j] = 0;
+        if (sum < 2 || sum > 3) {
+          cellsArray[i][j] = 0;
+          drawCell(canvasElement, i, j, 0, cellSize);
+        }
       } else {
-        if (sum == 3) cellsArray[i][j] = 1;
+        if (sum == 3) {
+          cellsArray[i][j] = 1;
+          drawCell(canvasElement, i, j, 1, cellSize);
+        }
       }
     }
   }
 }
 
-function doStep(cellsArray, canvasElement, step) {
-  nextPopulation(cellsArray);
-  drawCells(cellsArray, canvasElement, step);
-}
-$('#stepButton').click(function() {
-  doStep(lifeCells, $('#lifeCanvas')[0], STEP);
-});
+function drawCell(canvasElement, iPos, jPos, isAlive, cellSize) {
+  var ctx = canvasElement.getContext('2d');
+  var width = canvasElement.width;
+  var height = canvasElement.height;
+  //Calculate center of drawing circle
+  var x = iPos * cellSize + cellSize / 2;
+  var y = jPos * cellSize + cellSize / 2;
 
-$('#playButton').click(function() {
-  if (!timer) {
-    timer = setTimeout(function tick() {
-      doStep(lifeCells, $('#lifeCanvas')[0], STEP);
-      timer = setTimeout(tick, 100 / $('#speedInput').val());
-    }, 100 / $('#speedInput').val());
-  } else {
-    clearTimeout(timer);
-    timer = null;
-  }
-});
+  //Clear existing image
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(x - cellSize / 2, y - cellSize / 2, cellSize, cellSize);
+
+  //Draw new image
+  ctx.lineWidth = 1;
+  //gradient from RGB(0, 130, 130) to RGB(0, 230, 230)
+  ctx.strokeStyle = 'rgb(0,' + Math.floor(100 * x / width + 130) + ',' + Math.floor(100 * y / height + 130) + ')';
+  ctx.fillStyle = isAlive ? ctx.strokeStyle : '#fff';
+  ctx.beginPath();
+  ctx.arc(x, y, cellSize * 0.42, 0, 2 * Math.PI, true);
+  ctx.fill();
+  ctx.stroke();
+}
