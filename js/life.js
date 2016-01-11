476bb422a43ca3c711bfdd7dee69b77325805b24
Felytic <felytic@gmail.com>
2016-01-11 04:56:59 +0200

    new features

--- js/life.js
+++ js/life.js
@@ -2,13 +2,26 @@
 var lifeCells = [
   [0]
 ];
-var CELL_SIZE = 20;
+var CELL_SIZE = 10;
 var timer;
+var cyclical = true;
 
 $(window).load(function() {
   resizeCanvas($('#lifeCanvas')[0], CELL_SIZE);
   fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
   drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  for (var f in figures) {
+    $('#figuresList').append('<li><a href ="#">' + f + '</a></li>');
+  }
+  $('#figuresList li a').click(function() {
+    var key = $(this).html();
+    lifeCells = new Array(figures[key].length);
+    for (var i = 0; i < figures[key].length; i++) {
+      lifeCells[i] = figures[key][i].slice();
+    }
+    fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+    drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  });
 });
 
 $(window).resize(function() {
@@ -22,14 +35,19 @@ $('.play-pause').click(function() {
   $(this).find(':first-child').toggleClass('glyphicon-pause');
 });
 
+$('#worldButton').click(function() {
+  $(this).toggleClass('active');
+  cyclical = !cyclical;
+});
+
 $('#playButton').click(function() {
   if (!timer) {
     timer = setTimeout(function tick() {
-      nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
-      timer = setTimeout(tick, 500 / $('#speedInput').val());
+      nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE, cyclical);
+      timer = setTimeout(tick, 1000 / $('#speedInput').val());
       // "500 / $('#speedInput').val()" duplicates because it should be calculated on each step
       //              ^ this value can be changed by user while animation
-    }, 500 / $('#speedInput').val());
+    }, 1000 / $('#speedInput').val());
   } else {
     clearTimeout(timer);
     timer = null;
@@ -46,15 +64,15 @@ $('#lifeCanvas').click(function(pos) {
   y = y - y % CELL_SIZE + CELL_SIZE / 2;
 
   //Position in cells matrix
-  var i = Math.floor(x / CELL_SIZE);
-  var j = Math.floor(y / CELL_SIZE);
+  var i = Math.floor(y / CELL_SIZE);
+  var j = Math.floor(x / CELL_SIZE);
 
   lifeCells[i][j] = +!lifeCells[i][j];
   drawCell(this, i, j, lifeCells[i][j], CELL_SIZE);
 });
 
 $('#stepButton').click(function() {
-  nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  nextPopulation(lifeCells, $('#lifeCanvas')[0], CELL_SIZE, cyclical);
 });
 
 $('#resetButton').click(function() {
@@ -65,16 +83,36 @@ $('#resetButton').click(function() {
   drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
 });
 
+$('#sizeInput').change(function() {
+  CELL_SIZE = $(this).val();
+  resizeCanvas($('#lifeCanvas')[0], CELL_SIZE);
+  fitCellsToCanvas(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+  drawCells(lifeCells, $('#lifeCanvas')[0], CELL_SIZE);
+});
+
 function fitCellsToCanvas(cellsArray, canvasElement, cellSize) {
   var cells = cellsArray;
   var cellsWidth = canvasElement.width / cellSize;
   var cellsHeight = canvasElement.height / cellSize;
-  if (cellsWidth > cells.length || cellsHeight > cells[0].length) {
+
+  if (cellsHeight > cells.length) {
+    cells.push.apply(cells, Array.apply(null, new Array(cellsHeight - cells.length)).map(
+      e => Array.apply(null, new Array(cellsWidth)).map(e => 0)));
+  }
+
+  if (cellsWidth > cells[0].length) {
     for (var i = 0; i < cells.length; i++) {
-      cells[i].push.apply(cells[i], Array.apply(null, new Array(cellsHeight - cells[i].length)).map(e => 0));
+      cells[i].push.apply(cells[i], Array.apply(null, new Array(cellsWidth - cells[i].length)).map(e => 0));
     }
-    cells.push.apply(cells, Array.apply(null, new Array(cellsWidth - cells.length)).map(
-      e => Array.apply(null, new Array(cellsHeight)).map(e => 0)));
+  }
+  if (cellsHeight < cells.length) {
+    cells.splice(cellsHeight, cells.length - cellsHeight);
+  }
+
+  if (cellsWidth < cells[0].length) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].splice(cellsWidth, cells[i].length - cellsWidth);
+    }
   }
 }
 
@@ -86,25 +124,33 @@ function drawCells(cellsArray, canvasElement, cellSize) {
   }
 }
 
-function resizeCanvas(canvasElement, step) {
+function resizeCanvas(canvasElement, cellSize) {
   var canvas = $(canvasElement);
   var container = canvas.parent();
-  canvas.height(container.height() - canvas.position().top - step);
-  canvas.height(canvas.height() - canvas.height() % step);
+  canvas.height(container.height() - canvas.position().top - cellSize);
+  canvas.height(canvas.height() - canvas.height() % cellSize);
   canvas.width(container.width());
-  canvas.width(canvas.width() - canvas.width() % step);
+  canvas.width(canvas.width() - canvas.width() % cellSize);
   canvas[0].width = canvas.width();
   canvas[0].height = canvas.height();
 }
 
-function nextPopulation(cellsArray, canvasElement, cellSize) {
+function nextPopulation(cellsArray, canvasElement, cellSize, isCyclical) {
   var cells = cellsArray.map(e => e.slice());
   for (var i = 0; i < cells.length; i++) {
     for (var j = 0; j < cells[0].length; j++) {
       var sum = 0;
-      for (var x = i - 1; x <= i + 1; x++) {
-        for (var y = j - 1; y <= j + 1; y++) {
-          if (cells[x] && cells[x][y]) sum++;
+      for (var y = i - 1; y <= i + 1; y++) {
+        for (var x = j - 1; x <= j + 1; x++) {
+          if (isCyclical) {
+            var a = y < 0 ? y + cells.length : y;
+            var b = x < 0 ? x + cells[i].length : x;
+            a = a >= cells.length ? a - cells.length : a;
+            b = b >= cells[i].length ? b - cells[i].length : b;
+            if (cells[a][b]) sum++;
+          } else {
+            if (cells[y] && cells[y][x]) sum++;
+          }
         }
       }
       sum -= cells[i][j];
@@ -128,8 +174,8 @@ function drawCell(canvasElement, iPos, jPos, isAlive, cellSize) {
   var width = canvasElement.width;
   var height = canvasElement.height;
   //Calculate center of drawing circle
-  var x = iPos * cellSize + cellSize / 2;
-  var y = jPos * cellSize + cellSize / 2;
+  var y = iPos * cellSize + cellSize / 2;
+  var x = jPos * cellSize + cellSize / 2;
 
   //Clear existing image
   ctx.fillStyle = '#fff';
